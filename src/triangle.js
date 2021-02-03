02a90edc332a825129e48429a410c6c70fe06944
Sage Sypert <sage.sypert@gmail.com>
2021-02-02 19:38:53 -0800

    add class to triangle.js

--- src/triangle.js
+++ src/triangle.js
@@ -1,10 +1,11 @@
-export default function Triangle(side1, side2, side3) {
+class Triangle {
+  constructor(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
 }
 
-Triangle.prototype.checkType = function() {
+checkType() {
   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
